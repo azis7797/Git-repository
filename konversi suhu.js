beb4f04639e6fc9c9e9f4e24f337a66486fad69a
NUR AZIS <nurazis070797@gmail.com>
2025-03-15 20:07:09 +0900

    Add files via upload

---
+++ konversi suhu.js
@@ -0,0 +1,5 @@
+// Buat program untuk mengubah suhu dari farhenheit ke celcius
+function konversiSuhu(fahrenheit) {
+  return (fahrenheit - 32) * 5/9;
+}
+console.log(konversiSuhu(100)); // 37.77777777777778
